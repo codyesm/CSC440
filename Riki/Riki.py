51f75147e30a5caea38846be79be3c98915191d4
Sarah Nottingham <45541735+sarahnicole729@users.noreply.github.com>
2020-04-26 23:02:29 -0400

    Code to add Calendar API

--- Riki/Riki.py
+++ Riki/Riki.py
@@ -1,9 +1,33 @@
-#!/Users/smcho/virtualenv/riki/bin/python
+#!/Users/snnot/Desktop/se_project_env
 
 # -*- coding: utf-8 -*-
 import os
 
 from wiki import create_app
+"""
+import calendar
+
+print(calendar.weekheader(3))
+print()
+
+print(calendar.month(2020,4))
+"""
+
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+scopes = ['https://www.googleapis.com/auth/calendar']
+
+flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+credentials = flow.run_console()
+
+#pickle.dump(credentials, open("token.pkl", "wb"))
+#credentials = pickle.load(open("token.pkl", "rb"))
+
+service = build("calendar", "v3", credentials=credentials)
+
+result = service.calendarList().list().execute()
+
 
 directory = os.getcwd()
 app = create_app(directory)
